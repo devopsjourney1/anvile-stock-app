5e7eb1a2a1f092f7687fde26b441094b2891dab7
devopsjourney1 <34145011+devopsjourney1@users.noreply.github.com>
2021-09-20 19:27:25 -0700

    Update Form1.py

--- Form1.py
+++ Form1.py
@@ -1,12 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
 
 class Form1(Form1Template):
 
@@ -14,13 +11,20 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.drop_down_stocks.items = anvil.server.call('getTickers')
-
+    
     # Any code you write here will run when the form opens.
 
-  def drop_down_1_change(self, **event_args):
+  def drop_down_stocks_change(self, **event_args):
     """This method is called when an item is selected"""
-    #alert('you changed the ticker to ' + self.drop_down_1.selected_value)
+    #alert('You selected a stock ' + self.drop_down_stocks.selected_value)
     ticker = self.drop_down_stocks.selected_value
-    self.tickerName.text = ticker
-    price = anvil.server.call('getPrice', ticker)
-    self.tickerPrice.text = price
+    self.stockTicker.text = ticker
+    self.stockPrice.text = anvil.server.call('getPrice', ticker)
+
+  def drop_down_stocks_show(self, **event_args):
+    """This method is called when the DropDown is shown on the screen"""
+    ticker = self.drop_down_stocks.selected_value
+    self.stockTicker.text = ticker
+    self.stockPrice.text = anvil.server.call('getPrice', ticker)
+
+
